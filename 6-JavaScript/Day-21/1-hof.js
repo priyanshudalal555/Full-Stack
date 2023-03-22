c8723be6d8096f9c0c20c81abb4e4968c90ecc0d
priyanshu <priyanshudalal75@gmail.com>
2023-03-22 09:57:48 +0530

    try and catch

--- 6-JavaScript/Day-21/1-hof.js
+++ 6-JavaScript/Day-21/1-hof.js
@@ -55,7 +55,35 @@ console.log(country2.sort());
 console.log(country2.reverse()); 
 
 
-//Destructuring, Spread, and Rest
+//Destructuring
 const sci = [2.72, 3.14, 9.81, 37, 100];
 let[e, pi, grav, bodytemp, boiltemp] = sci;
 console.log(e, pi, grav, bodytemp, boiltemp);
+
+//Spread
+function sum3(x, y, z){
+    return x+y+z;
+}
+let vls = [1,2,3];
+console.log(sum3(...vls));
+
+//Rest
+function sums(...args){
+    let sum = 0;
+    for (const x of args){
+        sum= sum+x;
+    }
+    return sum;
+}
+console.log(sums(1,2,3,4,5,6,7,8,9));
+
+//Try and Catch
+try{
+    let fname = "Priyanshu";
+    console.log(fname + "" + lname);
+}catch (err) {
+    console.log(err.name, "Last Name Empty");
+}finally{
+    console.log("Run at any cost");
+}
+
